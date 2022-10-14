60041833bb332b3994374e769e281c2ff269011d
tracer-nju <tracer@nju.edu.cn>
2022-10-14 19:54:13 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: e2272d2c5ffe45169719aa8bf76c3774   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  19:54:14 up 39 min,  1 user,  load average: 0.01, 0.01, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,68 @@
 #include <stdint.h>
 
+uint64_t mod(uint64_t a, uint64_t b)
+{
+  while(a>b)
+	{
+		a=a-b;
+	}
+	return a;
+}
+
+uint64_t power_of_2(uint64_t a)
+{
+	uint64_t it=1;
+  if(a==0)
+		return it;
+	else
+	{
+		for(int i=0;i<a;i++)
+		{
+			it=it*2;
+		}
+		return it;
+	}
+}
+
+uint64_t mod_add(uint64_t a, uint64_t b, uint64_t m)
+{
+  uint64_t rest=power_of_2(32)-a;
+	if(rest>=b)
+	{
+		uint64_t c=a+b;
+		uint64_t x=mod(c,m);
+		uint64_t y=mod(power_of_2(32),m);
+		return x+y;
+	}
+	else{
+		return mod(x+y,m);
+	}
+}
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  bool[64] a_bit;
+	bool[64] b_bit;
+	uint64_t c=mod(a,m);
+	uint64_t d=mod(b,m);
+  while(int i=0;i<64;i++)
+	{
+    a_bit[i]=(c>>i)&1;
+		b_bit[i]=(d>>i)&1;
+	}
+	uint64_t base=0;
+  for(int i=0;i<64;i++)
+	{
+		if(a_bit[i])
+		{
+			uint64_t times=power_of_2(i);
+			for(int i=0;i<times;i++)
+			{
+				base=mod_add(base,b,m);
+			}
+		}
+
+	}
+
+
+	return base
 }
