60acb64227aeb032b0c58d703f36fd5fb0d1084a
tracer-nju <tracer@nju.edu.cn>
2022-10-15 18:03:01 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: 878add3b7ac44d1d878ea44024690dba   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  18:03:01 up  2:25,  1 user,  load average: 0.37, 0.15, 0.04

--- multimod/main.c
+++ multimod/main.c
@@ -1,4 +1,4 @@
-/*#include <stdint.h>
+/*#include <stdint.h>//test for mod_add
 #include <stdio.h>
 #include <inttypes.h>
 
@@ -23,7 +23,7 @@ int main() {
 }*/
 
 
-#include <stdint.h>
+/*#include <stdint.h> //initial test
 #include <stdio.h>
 #include <inttypes.h>
 
@@ -35,7 +35,22 @@ void test(uint64_t a, uint64_t b, uint64_t m) {
 }
 
 int main() {
-  test(123, 456, 789);
-  test(123, 456, -1ULL);
   test(-2ULL, -2ULL, -1ULL); // should be 1
+}*/
+
+
+include <stdint.h>//test for mod
+#include <stdio.h>
+#include <inttypes.h>
+
+uint64_t mod(uint64_t, uint64_t);
+uint64_t power_of_2(uint64_t);
+
+void test(uint64_t a,uint64_t m) {
+  #define U64 "%" PRIu64
+  printf(U64 " mod " U64 " = " U64 "\n", a,  m, mod(a,  m));
+}
+
+int main() {
+  test(power_of_2(63), -1ULL); // 
 }
