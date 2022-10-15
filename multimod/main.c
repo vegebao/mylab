ca38481208af3e8d4501012c7d5e6af00d66a5d0
tracer-nju <tracer@nju.edu.cn>
2022-10-15 18:09:59 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: 878add3b7ac44d1d878ea44024690dba   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  18:09:59 up  2:32,  1 user,  load average: 0.09, 0.14, 0.08

--- multimod/main.c
+++ multimod/main.c
@@ -30,15 +30,21 @@ int main() {
 }*/
 
 
-include <stdint.h>//test for mod
+
+#include <stdint.h>//test for mod
 #include <stdio.h>
 #include <inttypes.h>
+uint64_t mod_add(uint64_t, uint64_t, uint64_t);
 uint64_t mod(uint64_t, uint64_t);
-uint64_t power_of_2(uint64_t);
 void test(uint64_t a,uint64_t m) {
   #define U64 "%" PRIu64
-  printf(U64 " mod " U64 " = " U64 "\n", a,  m, mod(a,  m));
+  printf(U64 " mod "  U64 " = " U64 "\n", a,m , mod(a,m));
 }
 int main() {
-  test(-1ULL, -1ULL); // 
+  test(15498,36);//<2^32;<2^32    shoubld be 0
+  test(4294156248,61);//<2^32;<2^32   should be 49
+  test(4294217854,4216548765);//<2^32  <2^32  should be 2265614747
+  test(4697541238,36);
+  test(4954871392,4265487912);
+  test(-1ULL,-1ULL,-2ULL);
 }
