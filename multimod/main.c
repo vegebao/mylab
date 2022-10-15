a7582d84f0f6f3f390d1964d8c77e75b4e9a6914
tracer-nju <tracer@nju.edu.cn>
2022-10-15 09:24:05 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: b8e3f2c51d574a1387245b86258c0aa9   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  09:24:05 up 59 min,  1 user,  load average: 0.11, 0.15, 0.12

--- multimod/main.c
+++ multimod/main.c
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-uint64_t mod(uint64_t, uint64_t);
+uint64_t multimod(uint64_t, uint64_t, uint64_t);
 
-void test(uint64_t a, uint64_t m) {
+void test(uint64_t a, uint64_t b, uint64_t m) {
   #define U64 "%" PRIu64
-  printf(U64 " mod " U64 " = " U64 "\n", a, m ,mod(a,m));
+  printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
 }
 
 int main() {
-  test(783, 456);
-  printf(-1ULL);
-  printf(-2ULL);
-  test(-1ULL ,-2ULL); 
+  test(123, 456, 789);
+  test(123, 456, -1ULL);
+  test(-2ULL, -2ULL, -1ULL); // should be 1
 }
