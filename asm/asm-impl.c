8a6097a7a06e82d576a44ae40a01ee794f35408a
tracer-nju <tracer@nju.edu.cn>
2023-01-25 19:23:53 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: cded466bb33a406d8ffa2717ea7cf696   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  19:23:53 up  1:30,  1 user,  load average: 0.12, 0.11, 0.05

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -11,10 +11,23 @@ int64_t asm_add(int64_t a, int64_t b) {
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
-  for (int i = 0; i < 64; i++) {
+  /*for (int i = 0; i < 64; i++) {
     if ((x >> i) & 1) s++;
-  }
+  }*/
+  int s=0,i=0;
+  asm(
+    "L1:\n\t"
+    "mov %%rdx, %%rdi\n\t"
+    "shr %%ecx, %%rdi\n\t"
+    "and $1, %%rdi\n\t"
+    "add %%edi %%eax\n\t"
+    "add $1, %%ecx\n\t"
+    "cmp $32, %ecx\n\t"
+    "jne L1\n\t"
+    :"=a"(s)
+    :"c"(i),"d"(x)
+    :"ecx","eax","rdi"
+  );
   return s;
 }
 
