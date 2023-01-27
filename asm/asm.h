d897771583dbd68711d4867c5e5e842d508c36f1
tracer-nju <tracer@nju.edu.cn>
2023-01-27 19:34:03 +0800

    Static hostname: zhaojw-virtual-machine        Icon name: computer-vm          Chassis: vm       Machine ID: 2e1ffb914aa84cc29eec92e07844dfd2          Boot ID: 9e05dfc3eadc4643a09068ed84d85058   Virtualization: vmware Operating System: Ubuntu 22.04.1 LTS           Kernel: Linux 5.15.0-50-generic     Architecture: x86-64  Hardware Vendor: VMware, Inc.   Hardware Model: VMware Virtual Platform  19:34:03 up  4:37,  1 user,  load average: 0.49, 0.68, 0.62

--- asm/asm.h
+++ asm/asm.h
@@ -3,7 +3,18 @@
 
 // TODO: replace with your definition
 #include <setjmp.h> // remove this
-#define asm_jmp_buf jmp_buf
+typedef struct JMP_BUF{
+  uint64_t rbx;
+  uint64_t rcx;
+  uint64_t rdx;
+  uint64_t rsp;
+  uint64_t rbp;
+  uint64_t rsi;
+  uint64_t rdi;
+  uint64_t rip;
+} my_jmp_buf;
+
+#define asm_jmp_buf my_jmp_buf
 
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
